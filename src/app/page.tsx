5cb9b7a43170db973b7c8b106dafa6fc373cf702
Ryan Kovlaske <ryan.kovlaske@gmail.com>
2023-09-25 09:05:57 -0700

    made ascii art work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   //USE STATE--------------------------------------------------------------------
   const [timeCurrent, setTimeCurrent] = useState(new Date().toLocaleTimeString())
-  const [timeStatic, setTimeStatic] = useState(new Date().toLocaleTimeString())
+  const [timeStatic, setTimeStatic] = useState(new Date().toLocaleTimeString('en-US',{hour12: false}))
 
   //USE EFFECT--------------------------------------------------------------------
   useEffect(() => {
@@ -27,22 +27,32 @@ export default function Home() {
       <div className="pastLine flex">
         <div className="time w-24 ">[{timeStatic}]</div>
         <div className="name w-24 text-right">Client &nbsp;</div>
-        <div className="w-full text">  System Running...</div>
+        <div className="w-full text whitespace-pre">  
+            <code>{` __   __  _______  ___      __   __    _______  ___   __   __  _______ 
+|  | |  ||       ||   |    |  | |  |  |       ||   | |  | |  ||       |
+|  |_|  ||   _   ||   |    |  |_|  |  |    ___||   | |  |_|  ||    ___|
+|       ||  | |  ||   |    |       |  |   |___ |   | |       ||   |___ 
+|       ||  |_|  ||   |___ |_     _|  |    ___||   | |       ||    ___|
+|   _   ||       ||       |  |   |    |   |    |   |  |     | |   |___ 
+|__| |__||_______||_______|  |___|    |___|    |___|   |___|  |_______|
+
+To Start press 1`}</code>
+        </div>
       </div>
 
       
-      <div className="editline bg-slate-950 flex">
+      <div className="editline bg-zinc-800 flex">
         <div className="time w-24 ">[{timeCurrent}]</div>
         <div className="nameCurrent w-24 text-right">&gt; &nbsp;</div>
-        <input className="w-full appearance-none text bg-slate-950 outline-none "></input>
+        <input className="w-full appearance-none text bg-zinc-800 outline-none "></input>
       </div>
     </main>
   )
 }
 /*
               O
-              {______________________________________
+              |______________________________________
 @XXXXXXXXXXXXXX_____________HOLY FIVE_______________/
-              {
+              |
               O
 */
